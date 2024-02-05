dd9354a18407b308b5d0951e34ec8742a0705e2a
Benediktas Rukas <39025147+benycode@users.noreply.github.com>
2024-02-05 17:19:50 +0200

    Update App.php

--- src/App.php
+++ src/App.php
@@ -6,10 +6,14 @@ namespace BenyCode\Slim\Abstraction;
 
 use DI\ContainerBuilder;
 use Slim\App as SlimApp;
+use React\EventLoop\Loop;
+use React\Socket\SocketServer;
+use React\Http\Server as HttpServer;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 final class App
 {
-    public static function run(array $actionPath, array $dependencies) : SlimApp
+    public static function run(array $actionPath, array $dependencies) : void
     {
         $container = (new ContainerBuilder)
             ->addDefinitions(
@@ -18,17 +22,30 @@ final class App
                     $dependencies,
                 ),
             )
-            ->build()
-        ;
+            ->build();
 
         $app = $container
-            ->get(SlimApp::class)
-        ;
+            ->get(SlimApp::class);
 
-        $app
-            ->run()
-        ;
+        $loop = Loop::get();
 
-        return $app;
+        $server = new HttpServer(
+            function (Request $request) use ($app) {
+                return $app
+                    ->handle($request);
+            }
+        );
+
+        $uri = \getenv('HTTP_SERVER_URI') ? \getenv('HTTP_SERVER_URI') : '0.0.0.0:80';
+
+        $socket = new SocketServer($uri);
+
+        $server
+            ->listen($socket);
+
+        echo \sprintf('Server running at http://%s', $uri) . PHP_EOL;
+
+        $loop
+            ->run();
     }
 }
